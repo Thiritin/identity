1557096790f88d2534c935acabd8962043481bc1
Thiritin <me@thiritin.com>
2021-07-12 00:56:20 +0200

    Add Facivon and Title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>Eurofurence Identity</title>
+
+        <!-- Favicon -->
+        <link rel="icon" type="image/svg" href="/favicon.svg">
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
