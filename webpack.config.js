dcf50ab8685d11259a4d103f56d7890fcdc5a22b
Thiritin <me@thiritin.com>
2022-12-03 18:06:16 +0100

    add mocked webpack.config.js for autocompletion in phpstorm

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path')
+
+module.exports = {
+    output: {chunkFilename: 'js/[name].js?id=[chunkhash]'},
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.runtime.esm.js',
+            '@': path.resolve('./resources/js'),
+        },
+    },
+}
