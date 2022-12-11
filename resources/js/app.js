8ebd4424d052636463a9406502b1cba7c298caf7
Thiritin <me@thiritin.com>
2022-12-11 10:56:05 +0100

    fix admin

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,20 +2,24 @@ import './bootstrap';
 import '../css/app.css';
 
 // Import modules...
-import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/inertia-vue3';
-import { InertiaProgress } from '@inertiajs/progress';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
-import { __, getLocale, locales, setLocale, trans, transChoice } from 'matice'
+import {createApp, h} from 'vue';
+import {createInertiaApp} from '@inertiajs/inertia-vue3';
+import {InertiaProgress} from '@inertiajs/progress';
+import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers';
+import {ZiggyVue} from '../../vendor/tightenco/ziggy/dist/vue.m';
+import {__, getLocale, locales, setLocale, trans, transChoice} from 'matice'
+
+import.meta.glob([
+    '../assets/**',
+]);
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Identity';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
-    setup({ el, app, props, plugin }) {
-        return createApp({ render: () => h(app, props) })
+    setup({el, app, props, plugin}) {
+        return createApp({render: () => h(app, props)})
             .use(plugin)
             .use(ZiggyVue, Ziggy)
             .mixin({methods: {route}})
