5f04b414ab53434958fdda3276e57616b201ef11
Thiritin <me@thiritin.com>
2023-02-06 00:22:15 +0100

    Add proper error page for oidc

---
+++ resources/js/Pages/Auth/Error.vue
@@ -0,0 +1,35 @@
+<template>
+    <auth-layout>
+        <Logo></Logo>
+        <LoginScreenWelcome
+            :title="title"
+            class="mb-10"
+        />
+        <div>{{ description }}</div>
+    </auth-layout>
+</template>
+
+<script>
+import Logo from '@/Auth/Logo.vue'
+import LoginScreenWelcome from '@/Auth/LoginScreenWelcome.vue'
+import AuthLayout from '@/Layouts/AuthLayout.vue'
+
+export default {
+    components: {
+        AuthLayout,
+        Logo,
+        LoginScreenWelcome,
+    },
+
+    props: {
+        title: String,
+        description: String
+    },
+
+    methods: {
+        submit() {
+            this.form.post(this.route('verification.send'))
+        },
+    },
+}
+</script>
