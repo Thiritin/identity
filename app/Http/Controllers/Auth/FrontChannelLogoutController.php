2b5818e542c41a9cf98ba8621d6dc35782fa0cdd
Thiritin <me@thiritin.com>
2023-02-13 22:54:30 +0100

    fixes #50

--- app/Http/Controllers/Auth/FrontChannelLogoutController.php
+++ app/Http/Controllers/Auth/FrontChannelLogoutController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Redirect;
 
 class FrontChannelLogoutController extends Controller
 {
     public function __invoke(Request $request)
     {
         Auth::guard('web')->logout();
-        return "Logout successful.";
+        return Redirect::route('auth.choose');
     }
 }
