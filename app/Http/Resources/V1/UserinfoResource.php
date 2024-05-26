660d02527bede69d64dc0b224d40263e1fa2c037
Thiritin <me@thiritin.com>
2024-05-26 17:29:03 +0200

    add nextcloud info to idp

--- app/Http/Resources/V1/UserinfoResource.php
+++ app/Http/Resources/V1/UserinfoResource.php
@@ -30,17 +30,28 @@ class UserinfoResource extends JsonResource
         $data = [];
 
         $data['sub'] = $this->hashid;
+        $this->loadMissing('groups');
+
         if ($this->scopeCheck('email')) {
             $data['email'] = $this->email;
             $data['email_verified'] = !is_null($this->email_verified_at);
         }
+
         if ($this->scopeCheck('profile')) {
             $data['name'] = $this->name;
+
             $data['avatar'] = ($request->user()->profile_photo_path) ? Storage::disk('s3-avatars')->url($request->user()->profile_photo_path) : null;
         }
         if ($this->whenLoaded('groups') && $this->scopeCheck('groups')) {
             $data['groups'] = $this->groups->filter(fn(Group $group
             ) => $group->pivot->level !== GroupUserLevel::Invited)->pluck('hashid');
+        }
+        /**
+         * APP Specific: NEXTCLOUD
+         */
+        if ($this->scopeCheck('nextcloud')) {
+            $data['nextcloud_groups'] = $this->groups->whereNotNull('nextcloud_folder_name')->pluck('nextcloud_folder_name');
+            $data['nextcloud_admin'] = $this->groups->contains('system_name', 'nextcloud_admins');
         }
 
         return $data;
