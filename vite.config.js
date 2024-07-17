25289db76fe745cc12f7231a9ab94e31d1f18b20
Thiritin <me@thiritin.com>
2024-07-18 00:02:10 +0200

    fix build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    assetsInclude: ['**/*.TTF'],
     plugins: [
         laravel({
             input: 'resources/js/app.js',
