6b80d2e4361fb9dcffa6c558297661dc776aed3c
Tin <me@thiritin.com>
2026-04-02 07:23:47 +0200

    Add create new account link below login button

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -54,6 +54,12 @@
             type="submit"
             class="w-full"
         >{{ $trans('sign_in') }} <ArrowRight class="size-4" /></Button>
+        <Link
+            :href="route('auth.register.view')"
+            class="block text-center text-xs text-gray-500 hover:text-gray-700 dark:text-primary-400 dark:hover:text-primary-300"
+        >
+            {{ $trans('choose_create_new_account') }}
+        </Link>
     </form>
 </template>
 <script setup>
