fd5fccd35c4d12a92fd79676a4da45be0fccca67
Tin <me@thiritin.com>
2026-04-02 07:09:56 +0200

    Reduce spacing below logo

--- resources/js/Auth/LoginScreenWelcome.vue
+++ resources/js/Auth/LoginScreenWelcome.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mt-12">
+  <div class="mt-4">
     <h1 class="text-primary-500 font-semibold text-5xl dark:text-primary-300">{{ title }}</h1>
     <h2 class="text-gray-600 dark:text-primary-400 text-3xl">{{ subTitle }}</h2>
   </div>
