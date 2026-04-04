214ce0e1aa1085aff7c847857e052ee2ef1b388a
Tin <me@thiritin.com>
2026-04-05 00:04:19 +0200

    Expose expires_at in metadata resource

--- app/Http/Resources/V2/MetadataResource.php
+++ app/Http/Resources/V2/MetadataResource.php
@@ -14,6 +14,7 @@ class MetadataResource extends JsonResource
         return [
             'key' => $this->key,
             'value' => $this->value,
+            'expires_at' => $this->expires_at?->toIso8601String(),
         ];
     }
 }
