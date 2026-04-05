fc4650f29d12d3b5b5a43039e0ac736c6fcad118
Tin <me@thiritin.com>
2026-04-06 01:19:06 +0200

    feat: add DevHashid component and i18n strings

---
+++ resources/js/Components/DevHashid.vue
@@ -0,0 +1,19 @@
+<template>
+    <span
+        v-if="devMode.enabled && id"
+        class="text-xs font-mono text-muted-foreground ml-2 select-all"
+        :title="id"
+    >
+        {{ id }}
+    </span>
+</template>
+
+<script setup>
+import { useDevMode } from '@/Composables/useDevMode'
+
+defineProps({
+    id: { type: String, default: null },
+})
+
+const devMode = useDevMode()
+</script>
