6baebe75a39be45afdb96a0f2e3449a6c22dc392
Tin <me@thiritin.com>
2026-04-05 23:23:49 +0200

    feat: add general-purpose s3 disk

--- config/filesystems.php
+++ config/filesystems.php
@@ -53,6 +53,18 @@ return [
             'use_path_style_endpoint' => true,
         ],
 
+        's3' => [
+            'driver' => 's3',
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION'),
+            'bucket' => env('AWS_BUCKET'),
+            'url' => env('AWS_URL'),
+            'endpoint' => env('AWS_ENDPOINT'),
+            'use_path_style_endpoint' => true,
+            'visibility' => 'public',
+        ],
+
     ],
 
     /*
