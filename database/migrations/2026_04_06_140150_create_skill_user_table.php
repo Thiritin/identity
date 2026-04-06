e3edd7f7b4a11fb9c5d68b711dc95e981e4f5cea
Tin <me@thiritin.com>
2026-04-06 16:02:12 +0200

    feat: add skills and skill_user tables

---
+++ database/migrations/2026_04_06_140150_create_skill_user_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('skill_user', function (Blueprint $table) {
+            $table->foreignId('skill_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->unique(['skill_id', 'user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('skill_user');
+    }
+};
